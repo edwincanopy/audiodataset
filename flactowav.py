af7782ff6deca1ade3d465ba7479cf780f5d14c6
edwincanopy <edwin@canopylabs.ai>
2024-08-15 09:40:13 +0100

    Update flactowav.py

--- flactowav.py
+++ flactowav.py
@@ -3,15 +3,18 @@ from pydub import AudioSegment
 
 READ_FILES = '.'
 SAVE_FILES = "./wav_files"
+BATCH_SIZE = 100
+
 
 def flac_to_wav():
+    os.makedirs(SAVE_FILES, exist_ok=True)
 
-    for filename in os.listdir(READ_FILES):
+    flac_files = [f for f in os.listdir(READ_FILES) if f.endswith(".flac")]
 
-        if filename.endswith(".flac"):
-
+    for i in range(0, len(flac_files), BATCH_SIZE):
+        batch_files = flac_files[i:i + BATCH_SIZE]
+        for filename in batch_files:
             flac_path = os.path.join(READ_FILES, filename)
-            
             wav_filename = filename.replace(".flac", ".wav")
             wav_path = os.path.join(SAVE_FILES, wav_filename)
             
